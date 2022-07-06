175c52faf43d260df51ac845073fdfd6a4d4159e
Pronicio <paolo0803.giometti@gmail.com>
2022-07-06 17:03:18 +0200

    ✨ Search persons

--- front/src/components/Contacts.vue
+++ front/src/components/Contacts.vue
@@ -2,9 +2,11 @@
   <section>
     <div class="search_bar">
       <div id="burger"></div>
-      <div id="search">
-        <div class="search_icon"></div>
-        <input type="search" id="site-search" placeholder="Search...">
+      <form v-on:submit.prevent="search" id="search">
+        <div class="search_icon" @click="search"></div>
+        <input type="search" id="site-search" placeholder="Search..." v-model="searchInput">
+      </form>
+      <div>
       </div>
     </div>
     <div class="contacts">
@@ -20,34 +22,60 @@
 </template>
 
 <script>
+import stringSimilarity from "string-similarity";
+
 export default {
   name: "Contacts",
   data: function () {
     return {
+      searchInput: null,
       current: null,
       contacts: [
-        { username: "Global Chat", id: "global", picture: "https://i.imgur.com/BcKjFGH.png", last: {
+        {
+          username: "Global Chat", id: "global", picture: "https://i.imgur.com/BcKjFGH.png", last: {
             msg: "Hello Everyone !", time: "Just now"
-        }},
-        { username: "Jessica Drew", id: "123", picture: "https://i.imgur.com/kPrwrVR.jpg", last: {
-          msg: "Ok, see you later", time: "Just now"
-        }},
-        { username: "David Moore", id: "123", picture: "https://i.imgur.com/SrNqmCr.jpg", last: {
+          }
+        },
+        {
+          username: "Jessica Drew", id: "123", picture: "https://i.imgur.com/kPrwrVR.jpg", last: {
+            msg: "Ok, see you later", time: "Just now"
+          }
+        },
+        {
+          username: "David Moore", id: "123", picture: "https://i.imgur.com/SrNqmCr.jpg", last: {
             msg: "You: i don't remember anything 😄", time: "18:30"
-        }},
-        { username: "Greg James", id: "123", picture: "https://i.imgur.com/CMow1x0.jpg", last: {
+          }
+        },
+        {
+          username: "Greg James", id: "123", picture: "https://i.imgur.com/CMow1x0.jpg", last: {
             msg: "I got a job at SpaceX 🎉 🚀 ", time: "18:16"
-        }},
-        { username: "Emily Dorson", id: "123", picture: "https://i.imgur.com/LuurvEM.jpg", last: {
+          }
+        },
+        {
+          username: "Emily Dorson", id: "123", picture: "https://i.imgur.com/LuurvEM.jpg", last: {
             msg: "Table for four, 5PM. Be there.", time: "18:02"
-        }},
-        { username: "Little Sister", id: "123", picture: "https://i.imgur.com/Q7Rxmwy.jpg", last: {
+          }
+        },
+        {
+          username: "Little Sister", id: "123", picture: "https://i.imgur.com/Q7Rxmwy.jpg", last: {
             msg: "Tell mom i will be home for tea 💜 ", time: "16:42"
-        }},
-        { username: "Art Class", id: "123", picture: "https://i.imgur.com/q1ssPTY.png", last: {
+          }
+        },
+        {
+          username: "Art Class", id: "123", picture: "https://i.imgur.com/q1ssPTY.png", last: {
             msg: "Your projects are due at 8pm sharp! Otherwise you'll have to deal with me!", time: "Yesterday"
-        }}
+          }
+        }
       ]
+    }
+  },
+  methods: {
+    search: function () {
+      this.contacts.sort((a, b) => {
+        let compareA = stringSimilarity.compareTwoStrings(a.username.toLowerCase(), this.searchInput.toLowerCase())
+        let compareB = stringSimilarity.compareTwoStrings(b.username.toLowerCase(), this.searchInput.toLowerCase())
+        return (Math.abs(1-compareA) - Math.abs(1-compareB));
+      });
     }
   }
 }
