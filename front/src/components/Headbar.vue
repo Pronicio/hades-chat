0340d24bf9fc1c34e3a6e19a53be62b7cfa129fe
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-08-11 14:10:56 +0200

    💄 Fix height in the msg box
    + Other bugs

--- front/src/components/Headbar.vue
+++ front/src/components/Headbar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="headbar">
-    <div class="right">
+    <div class="left">
       <img :src="details.picture" alt="Picture" width="50" height="50" />
       <div class="infos">
         <h4>{{ details.username }}</h4>
         <p>{{ details.last.time }}</p>
       </div>
     </div>
-    <div class="left">
+    <div class="right">
       <div id="search"></div>
       <div id="call"></div>
       <div id="more"></div>
