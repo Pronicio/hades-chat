286255fd6ae9bdf2318d7d9eeef203a00ccd2d40
Pronicio <52421988+Pronicio@users.noreply.github.com>
2022-08-11 15:59:05 +0200

    🚧 Imgur picture

--- front/src/views/Connect.vue
+++ front/src/views/Connect.vue
@@ -10,18 +10,57 @@
         </div>
         <button @click="setName">Validate !</button>
       </form>
+      <div class="picture"
+           :style="`background: #fefefe url('${picture}') no-repeat; background-size: 14.375rem;`"></div>
     </div>
   </section>
 </template>
 
 <script>
+import { Dropzone } from "dropzone";
+
 export default {
   name: "Connect",
   data: function () {
     return {
       name: null,
-      save_session: false
+      save_session: false,
+      picture: "src/assets/images/icons/user-plus.svg"
     }
+  },
+  mounted() {
+    const dropzone = new Dropzone(".picture", {
+      url: "/",
+      uploadMultiple: false,
+      createImageThumbnails: false,
+      disablePreviews: true
+    });
+
+    dropzone.on("addedfile", async (file) => {
+      const b64 = await this.getBase64(file)
+
+      const headers = new Headers();
+      headers.append("Authorization", `Client-ID ${import.meta.env.VITE_IMGUR_CLIENT_ID}`);
+
+      const formData = new FormData();
+      formData.append("image", b64.substring(b64.indexOf('base64,') + 7));
+
+      const requestOptions = {
+        method: 'POST',
+        headers: headers,
+        body: formData,
+        redirect: 'follow'
+      };
+
+      fetch("https://api.imgur.com/3/image", requestOptions)
+          .then(response => response.json())
+          .then(res => {
+            console.log(res.data.link)
+            this.picture = res.data.link;
+            console.log(this.picture)
+          })
+          .catch(error => console.log('error', error));
+    });
   },
   methods: {
     setName() {
@@ -33,8 +72,21 @@ export default {
       const storage = this.save_session ? localStorage : sessionStorage
 
       storage.setItem('name', this.name)
+      if (this.picture.includes("imgur")) {
+        console.log(true)
+        storage.setItem('picture', this.picture)
+      }
+
       if (this.save_session) storage.setItem('save_session', this.save_session)
       this.$router.push('/')
+    },
+    getBase64(file) {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+      });
     }
   }
 }
