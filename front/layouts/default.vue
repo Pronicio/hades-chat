55ad82d888270b4de9cbdf2c49a3e18f69c4d771
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-05-04 22:24:01 +0200

    🚧 Begin front.

---
+++ front/layouts/default.vue
@@ -0,0 +1,61 @@
+<template>
+    <header>
+        <div class="brand">
+            <div class="logo"></div>
+            <h1>Hades-chat</h1>
+        </div>
+    </header>
+    <main>
+        <aside>
+            <h3>Contacts :</h3>
+            <div class="contacts">
+                <div class="contact" v-for="contact in contacts" :key="contact.id">
+                    <img :src="contact.avatar" :alt="`${contact.username}'s avatar.`" width="75">
+                    <div class="infos">
+                        <h4>{{ contact.username }}</h4>
+                    </div>
+                </div>
+            </div>
+        </aside>
+        <slot></slot>
+        <aside></aside>
+    </main>
+</template>
+
+<script setup lang="ts">
+
+const contacts = [
+    {
+        id: "345d6825-4514-46af-9ddd-8a3338f240ce",
+        username: "Pablo",
+        avatar: "https://i.imgur.com/U3yw7ft.jpeg",
+        badge: { who: "you", status: "send" }
+    },
+    {
+        id: "b40e9721-9021-403b-bbe8-08eb253077c1",
+        username: "Alice",
+        avatar: "https://i.imgur.com/XfCyTdg.jpeg",
+        badge: { who: "it", status: "unread", data: 3 }
+    },
+    {
+        id: "93ff0ca6-fe24-4cf6-91f0-bc996f53578b",
+        username: "Chris",
+        avatar: "https://i.imgur.com/qnT4TJZ.png",
+        badge: { who: "it", status: "read", data: 5 }
+    },
+    {
+        id: "b6b22914-e80e-4b69-9f18-937111582def",
+        username: "John",
+        avatar: "https://i.imgur.com/M0xD1by.png",
+        badge: { who: "it", status: "miss-call" }
+    }
+]
+</script>
+
+<style lang="scss">
+@import '../assets/style/style.scss';
+</style>
+
+<style scoped lang="scss">
+@import '../assets/style/layouts/default.scss';
+</style>
---
+++ front/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <section id="app">
+        <div class="chat">
+            <p>Hello guysssssssssssssssssssss !</p>
+        </div>
+        <div class="chat me">
+            <p>Hi !!</p>
+        </div>
+        <div class="chat">
+            <p>How are you ?</p>
+        </div>
+
+        <div class="input">
+            <input type="text" id="text" name="text" required size="10" placeholder="Say something...">
+            <div class="send-icon"></div>
+        </div>
+    </section>
+</template>
+
+<style scoped lang="scss">
+@import '../assets/style/pages/index.scss';
+</style>
