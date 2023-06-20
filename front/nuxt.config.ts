825f05c6cd3df34edd02e550ec78bf58c6e302a9
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-06-20 16:07:04 +0200

    🔧 Modify webSocketApi url : public url.

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     runtimeConfig: {
         public: {
-            webSocketApi: "ws://127.0.0.1:9000/ws"
+            webSocketApi: "wss://hades-chat-v2.onrender.com/ws" //Dev url : ws://127.0.0.1:9000/ws
         }
     },
     modules: [
