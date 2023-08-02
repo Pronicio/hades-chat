2da7ee20d642ccdb4824df7e2fb0d3ce9f55a032
Pronicio <52421988+Pronicio@users.noreply.github.com>
2023-08-02 14:30:53 +0200

    🔧 Don't flush db on restart + update prod server url.

--- back/events/redisEvents.ts
+++ back/events/redisEvents.ts
@@ -1,12 +1,10 @@
 import { consola } from "consola";
-import { redis } from "../index";
 
 export function connected() {
     consola.success("Redis connected")
 }
 export function ready() {
     consola.success("Redis ready to go !")
-    redis.flushdb()
 }
 export function error(error: Error) {
     consola.error("Redis got an error: ", error)
